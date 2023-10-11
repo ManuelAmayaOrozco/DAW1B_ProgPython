1b80caa83eade42cb33fea0bbd60dd78874547c6
mamaoro1405 <mamaoro1405@g.educaand.es>
2023-10-11 16:32:09 +0200

    Actualización Ejercicios U1 número 2

---
+++ EjerciciosU1/ejercicio1.29.py
@@ -0,0 +1,30 @@
+"""
+Algoritmo que soolicite nombre y edad.
+Inicio
+    Escribe "Dime tu nombre:"
+    Leer nom
+    Si (nom=="" or " ")
+        nom = "Desconocido"
+    edad = 0
+    Mientras(edad<0 or edad>125)
+        Escribe "Dime tu edad entre 0 y 125 años:"
+        Leer edad
+        Si (edad<0 or edad>125)
+            Escribe "Edad no válida."
+    tiemp = 125-edad
+    Escribir "Te llamas " + nom + " y tienes " + edad + " años, te quedan aún " + tiemp + " años por cumplir."
+Fin
+"""
+
+nom = input("Dime tu nombre: ")
+if (nom==""):
+    nom = "Desconocido"
+elif (nom==" "):
+    nom = "Desconocido"
+edad = int(-1)
+while (edad<0 or edad>125):
+    edad = int(input("Dime tu edad entre 0 y 125 años: "))
+    if (edad<0 or edad>125):
+        print("Edad no válida.")
+tiemp = 125 - edad
+print("Te llamas", nom, "y tienes", edad, "años, te quedan aún", tiemp, "años por cumplir.")
