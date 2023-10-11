321449a6df92a6637f28120505de8999a6cee8f9
mamaoro1405 <mamaoro1405@g.educaand.es>
2023-10-11 17:32:29 +0200

    Actualización Ejercicios U1 número 2

--- EjerciciosU1/ejercicio1.29.py
+++ EjerciciosU1/ejercicio1.29.py
@@ -3,13 +3,13 @@ Algoritmo que soolicite nombre y edad.
 Inicio
     Escribe "Dime tu nombre:"
     Leer nom
-    Si (nom=="" or " ")
+    Si (nom=="" or " ") entonces
         nom = "Desconocido"
     edad = 0
-    Mientras(edad<0 or edad>125)
+    Mientras(edad<0 or edad>125) hacer
         Escribe "Dime tu edad entre 0 y 125 años:"
         Leer edad
-        Si (edad<0 or edad>125)
+        Si (edad<0 or edad>125) entonces
             Escribe "Edad no válida."
     tiemp = 125-edad
     Escribir "Te llamas " + nom + " y tienes " + edad + " años, te quedan aún " + tiemp + " años por cumplir."
---
+++ EjerciciosU1/ejercicio1.30.py
@@ -0,0 +1,20 @@
+"""
+Algoritmo que pida un número de inicio, un incremento y un total de la serie.
+Inicio
+    Escribir "Dime un número:"
+    Leer num
+    inc = 0
+    tot = 0
+    Mientras (inc<=0 or tot<=0) hacer
+        Escribir "Dime un incremento:"
+        Leer inc
+        Escribir "Dime un total de la serie:"
+        Leer tot
+        Si (inc<=0 or tot<=0) entonces
+            Escribir "Error. Incremento/Total no válido."
+        SiNo
+            ser1 = num + inc
+            Mientras (num=/tot)
+                restser = ser1 + 
+            Escribir "SERIE => "
+"""
