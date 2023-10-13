eba79ebe92e09d80840c18b491865dd362b2d65c
ManuelAmayaOrozco <127980333+ManuelAmayaOrozco@users.noreply.github.com>
2023-10-13 18:07:54 +0200

    Add files via upload

--- EjerciciosU1/ejercicio1.30.py
+++ EjerciciosU1/ejercicio1.30.py
@@ -8,13 +8,36 @@ Inicio
     Mientras (inc<=0 or tot<=0) hacer
         Escribir "Dime un incremento:"
         Leer inc
-        Escribir "Dime un total de la serie:"
+        Escribir "Dime un total de veces a repetir de la serie:"
         Leer tot
         Si (inc<=0 or tot<=0) entonces
             Escribir "Error. Incremento/Total no válido."
         SiNo
-            ser1 = num + inc
-            Mientras (num=/tot)
-                restser = ser1 + 
-            Escribir "SERIE => "
+            cont = 0
+            ser = "SERIE => " + num + " - " + (num+inc)
+            Mientras (cont < tot)
+                num+=inc
+                ser = ser + " .. " + num
+                cont+=1
+            ser = ser + " - " + (num+inc)
+            Escribir ser
 """
+
+num = int(input("Dime un número: "))
+inc = int(0)
+tot = int(0)
+while (inc<=0 or tot<=0):
+    inc = int(input("Dime un incremento: "))
+    tot = int(input("Dime un total de veces a repetir de la serie: "))
+    if (inc<=0 or tot<=0):
+        print("Error. Incremento/Total no válido.")
+    else:
+        cont = int(1)
+        ser = "SERIE => {num}-{plus}".format(num = num,plus = (num + inc))
+        num += inc
+        while (cont < tot-2):
+            num += inc
+            ser = "{ser}..{num}".format(ser = ser,num = num)
+            cont += 1
+        ser = "{ser}-{plus}".format(ser = ser,plus = (num + inc))
+        print(ser)
